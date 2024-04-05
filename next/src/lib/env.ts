3988b75cd6989d5f7fc012ba5b435e69832a8891
Mitchell Christ <xzmaru@gmail.com>
2024-04-04 22:31:57 -0700

    update domain

--- next/src/lib/env.ts
+++ next/src/lib/env.ts
@@ -1,4 +1,4 @@
 export default process.env.NODE_ENV === 'development' ||
 	process.env.IS_STAGING === 'true'
 
-export const BASE_URL = 'https://pit-stop-studios.vercel.app'
+export const BASE_URL = 'https://pit-stop.studio'
